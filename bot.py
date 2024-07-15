d5b3b6cfff9bdfac520dcddf8253e8ec9c67f74c
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-07-15 23:16:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ name ="""
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name="Bot", 'Rin Nanakura'))
+            name="Bot", 'Rin Nanakura')
             api_id=APP_ID, '7403636496:AAGguUeAV_riwDHP4xVUl-ImB-8Qs01NZnY))
             plugins={
                 "root": "plugins"
